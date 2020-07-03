9235ed8911c57afadbd8505bbf6ae9c3d98d73fd
sandeep.singh <sandeep.singh@ISIPLMUMLP141>
2020-07-03 18:55:27 +0530

    added AspNetCoreHostingModel with Entity,Repositiory,Controller & View

---
+++ SaiDhamProject/SaiDhamEntity/Entity/IEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaiDhamEntity.Entity
+{
+    public class IEmployee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MidleName { get; set; }
+
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public double Salary { get; set; }
+    }
+}
--- SaiDhamProject/SaiDhamRepositiory/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SaiDhamRepositiory
-{
-    public class Class1
-    {
-    }
-}
